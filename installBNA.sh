8b4a0a93a39254255ff7f201f536e5bc52bb1e5b
shakeib98 <shakeib98@outlook.com>
2019-11-05 14:17:00 +0500

    bna version update

--- installBNA.sh
+++ installBNA.sh
@@ -1,7 +1,7 @@
 composer archive create -t dir -n .
 
-composer network install --card PeerAdmin@hlfv1 --archiveFile anoubis-network@0.3.4-deploy.0.bna
+composer network install --card PeerAdmin@hlfv1 --archiveFile anoubis-network@0.3.5-deploy.0.bna
 
-composer network start --networkName anoubis-network --networkVersion 0.3.4-deploy.0 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkAdmin.card
+composer network start --networkName anoubis-network --networkVersion 0.3.5-deploy.0 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkAdmin.card
 
 composer card import --file networkAdmin.card
